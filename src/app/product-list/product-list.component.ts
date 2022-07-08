25625c4eb5cd02c0d5be764687fcf3a0efbe18b6
Marcio Oliveira <marciolea@gmail.com>
2022-07-08 20:32:03 -0300

    atualização do aplicativo para exibir a lista de produtos

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,13 +11,7 @@ export class ProductListComponent {
   products = products;
 
   share() {
-    window.alert('The product has been shared!');
+    window.alert('O produto foi compartilhado!');
   }
 }
 
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
